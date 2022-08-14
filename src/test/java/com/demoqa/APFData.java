0d4d1e83ac8061607ae3da06e2735da59a549056
Dmitriy Lyubtsov <dlyubtsov@rvision.ru>
2022-08-15 02:25:14 +0500

    optimize code

--- src/test/java/com/demoqa/APFData.java
+++ src/test/java/com/demoqa/APFData.java
@@ -3,12 +3,13 @@ package com.demoqa;
 import com.github.javafaker.Faker;
 
 import java.text.SimpleDateFormat;
+import java.util.List;
 
 public class APFData {
     Faker faker = new Faker();
     SimpleDateFormat dateToString = new SimpleDateFormat("MM.dd.yyyy");
 
-    String[] subjects = {"Maths", "Physics", "Computer Science"};
+    List<String> subjects = List.of("Maths", "Physics", "Computer Science");
     String
             firstName = faker.name().firstName(),
             lastName = faker.name().lastName(),
@@ -21,23 +22,4 @@ public class APFData {
             currentAddress = faker.address().streetAddress(),
             state = "NCR",
             city = "Noida";
-
-    public APFData(String firstName, String lastName, String email, String gender, String mobile, String dateOfBirth,
-                   String[] subjects, String hobbies, String picture, String currentAddress, String state, String city) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.email = email;
-        this.gender = gender;
-        this.mobile = mobile;
-        this.dateOfBirth = dateOfBirth;
-        this.subjects = subjects;
-        this.hobbies = hobbies;
-        this.picture = picture;
-        this.currentAddress = currentAddress;
-        this.state = state;
-        this.city = city;
-    }
-
-    public APFData() {
-    }
 }
--- src/test/java/com/demoqa/APFPage.java
+++ src/test/java/com/demoqa/APFPage.java
@@ -6,11 +6,11 @@ import org.openqa.selenium.Keys;
 import java.io.File;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.Arrays;
 import java.util.Locale;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.executeJavaScript;
+import static com.demoqa.APFElements.*;
 
 public class APFPage {
 
@@ -20,54 +20,54 @@ public class APFPage {
     }
 
     public void fillFirstName(APFData data) {
-        APFElements.firstName.setValue(data.firstName);
+        firstName.setValue(data.firstName);
     }
 
     public void fillLastName(APFData data) {
-        APFElements.lastName.setValue(data.lastName);
+        lastName.setValue(data.lastName);
     }
 
     public void fillEmail(APFData data) {
-        APFElements.email.setValue(data.email);
+        email.setValue(data.email);
     }
 
     public void fillGender(APFData data) {
-        APFElements.gender.find(new ByText(data.gender)).click();
+        gender.find(new ByText(data.gender)).click();
     }
 
     public void fillMobile(APFData data) {
-        APFElements.mobile.setValue(data.mobile);
+        mobile.setValue(data.mobile);
     }
 
     public void fillDateOfBirth(APFData data) {
-        APFElements.dateOfBirth.sendKeys(Keys.CONTROL + "a");
-        APFElements.dateOfBirth.sendKeys(data.dateOfBirth + Keys.ENTER);
+        dateOfBirth.sendKeys(Keys.CONTROL + "a");
+        dateOfBirth.sendKeys(data.dateOfBirth + Keys.ENTER);
     }
 
     public void fillSubjects(APFData data) {
-        for (int i = 0; i < data.subjects.length; i++) {
-            APFElements.subjects.setValue(data.subjects[i]).pressEnter();
+        for (String subject : data.subjects) {
+            subjects.setValue(subject).pressEnter();
         }
     }
 
     public void fillHobbies(APFData data) {
-        APFElements.hobbies.find(new ByText(data.hobbies)).click();
+        hobbies.find(new ByText(data.hobbies)).click();
     }
 
     public void fillPicture(APFData data) {
-        APFElements.picture.uploadFile(new File("src/test/resources/" + data.picture));
+        picture.uploadFile(new File("src/test/resources/" + data.picture));
     }
 
     public void fillCurrentAddress(APFData data) {
-        APFElements.currentAddress.setValue(data.currentAddress);
+        currentAddress.setValue(data.currentAddress);
     }
 
     public void fillState(APFData data) {
-        APFElements.state.setValue(data.state).pressEnter();
+        state.setValue(data.state).pressEnter();
     }
 
     public void fillCity(APFData data) {
-        APFElements.city.setValue(data.city).pressEnter();
+        city.setValue(data.city).pressEnter();
     }
 
     public void fillAPFFormAndSubmit(APFData data) {
@@ -83,25 +83,26 @@ public class APFPage {
         fillCurrentAddress(data);
         fillState(data);
         fillCity(data);
-        APFElements.submitBtn.click();
+        submitBtn.click();
     }
 
     public void checkEquals(APFData data) {
-        APFElements.enteredStudentName.shouldHave(text(data.firstName + " " + data.lastName));
-        APFElements.enteredEmail.shouldHave(text(data.email));
-        APFElements.enteredGender.shouldHave(text(data.gender));
-        APFElements.enteredMobile.shouldHave(text(data.mobile));
+        enteredStudentName.shouldHave(text(data.firstName + " " + data.lastName));
+        enteredEmail.shouldHave(text(data.email));
+        enteredGender.shouldHave(text(data.gender));
+        enteredMobile.shouldHave(text(data.mobile));
 
         LocalDate date = LocalDate.parse(data.dateOfBirth, DateTimeFormatter.ofPattern("MM.dd.yyyy"));
         String changedDateOfBirth = date.format(DateTimeFormatter.ofPattern("dd MMMM,yyyy", new Locale("en")));
-        APFElements.enteredDateOfBirth.shouldHave(text(changedDateOfBirth));
+        enteredDateOfBirth.shouldHave(text(changedDateOfBirth));
 
-        APFElements.enteredSubject.shouldHave(text(Arrays.toString(data.subjects).replace("[", "")
+
+        enteredSubject.shouldHave(text(data.subjects.toString().replace("[", "")
                 .replace("]", "")));
 
-        APFElements.enteredHobbies.shouldHave(text(data.hobbies));
-        APFElements.enteredPicture.shouldHave(text(data.picture));
-        APFElements.enteredAddress.shouldHave(text(data.currentAddress));
-        APFElements.enteredStateAndCity.shouldHave(text(data.state + " " + data.city));
+        enteredHobbies.shouldHave(text(data.hobbies));
+        enteredPicture.shouldHave(text(data.picture));
+        enteredAddress.shouldHave(text(data.currentAddress));
+        enteredStateAndCity.shouldHave(text(data.state + " " + data.city));
     }
 }
