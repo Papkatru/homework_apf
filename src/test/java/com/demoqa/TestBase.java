6e72ac9b3c7d11aed3af843edf2a04cdfdef8963
Dmitriy Lyubtsov <dlyubtsov@rvision.ru>
2022-09-19 23:53:47 +0500

    do my lesson 11 homework

--- src/test/java/com/demoqa/TestBase.java
+++ src/test/java/com/demoqa/TestBase.java
@@ -6,25 +6,34 @@ import io.qameta.allure.Step;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
-import static com.demoqa.helper.Attachments.takeScreenshot;
-import static com.demoqa.helper.Attachments.takeSource;
+import static com.demoqa.helper.Attachments.*;
 
 public class TestBase {
 
     @BeforeAll
     @Step("Установка настроек браузера")
     static void setup() {
+        SelenideLogger.addListener("allureTest", new AllureSelenide());
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
+
+        Configuration.browserCapabilities = capabilities;
         Configuration.browserSize = "1920x1080";
         Configuration.browserPosition = "0x0";
         Configuration.baseUrl = "https://demoqa.com";
-        SelenideLogger.addListener("allureTest", new AllureSelenide());
     }
 
     @AfterEach
     @Step("Получить аттачменты")
     void getAttachments(){
         takeScreenshot();
-        takeSource();
+        addSource();
+        addHTMLSource();
+        addBrowserConsoleLog();
+        addVideo();
     }
 }
